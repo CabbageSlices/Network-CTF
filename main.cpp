508f6e99d8a67710e05ef47a5f25181ed4b5c9b1
Irelevance <nimesh.shrestha@hotmail.com>
2014-06-30 13:02:36 -0700

    added main file

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "SFML/Network.hpp"
+#include "SFML/System.hpp"
+#include <cmath>
+
+using namespace std;
+
+const float PI = 3.14159265;
+
+float degreesToRadians(const float& angleInDegrees) {
+
+    return angleInDegrees * PI / 180.f;
+}
+
+float radiansToDegrees(const float& angleInRadians) {
+
+    return angleInRadians * 180.f / PI;
+}
+
+int main()
+{
+    sf::UdpSocket client;
+
+    sf::Packet packet;
+    unsigned short port = 8080;
+    sf::IpAddress ip(sf::IpAddress::getLocalAddress());
+
+    while(true) {
+
+        client.send(packet, ip, port);
+        sf::sleep(sf::milliseconds(150));
+    }
+
+    return 0;
+}
